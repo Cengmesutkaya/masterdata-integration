dfc20b0db7f4bc9b803f6bbd1ec2b9b36462747e
Mesut Kaya <mesutkaya2000@gmail.com>
2021-05-19 10:18:40 +0300

    Update GenericResponse.cs

--- masterdata-integration/models/GenericResponse.cs
+++ masterdata-integration/models/GenericResponse.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace masterdata_integration.models
 {
     public class GenericResponse<T>
